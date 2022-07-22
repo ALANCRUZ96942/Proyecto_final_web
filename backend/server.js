3306a5a259eafe97cac45ca7ead1e0d54160999a
ALANCRUZ96942 <alan96942@gmail.com>
2022-07-21 22:03:27 -0500

    update: server init

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,71 @@ app.use(cors())
 
 
 
+var dummyCourses = [
+  {
+    id: 1,
+    slug: "js",
+    name: "Javascript",
+    description: "lorem"
+  },
+  { 
+    id: 2,
+    slug: "java",
+    name: "Java",
+    description: "lorem"
+  },
+  {
+    
+    id: 3,
+    slug: "c",
+    name: "C y C++",
+    description: "lorem"
+  }
+];
+
+
+app.get("/api/courses", function(req, res) {
+  return res.json(dummyCourses);
+});
+
+
+app.get("/api",(req,res) =>{
+  res.send("Bienvenido");
+});
+
+app.get("/api/courses/:course", function(req, res) {
+  var chosen = req.params.course;
+
+  console.log(chosen);
+
+  for (var i = 0; i < dummyCourses.length; i++) {
+    if (chosen === dummyCourses[i].slug) {
+      return res.json(dummyCourses[i]);
+    }
+  }
+
+  return res.json(false);
+});
+
+
+// Create New Course
+app.post("/api/courses", function(req, res) {
+ 
+  var newcourse = req.body;
+
+  // Using a RegEx Pattern to remove spaces from newcourse
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  newcourse.slug = newcourse.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newcourse);
+
+  dummyCourses.push(newcourse);
+
+  res.json(newcourse);
+});
+
+
+
 
 // Starts the server to begin listening
 // =============================================================
